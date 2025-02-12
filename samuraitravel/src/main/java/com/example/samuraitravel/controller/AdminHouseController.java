618f069595bb439e061ce8e3738d4f609d428213
kantataguchi <kanta170434@gmail.com>
2025-02-12 18:45:51 +0900

    2月12日分コミット

--- samuraitravel/src/main/java/com/example/samuraitravel/controller/AdminHouseController.java
+++ samuraitravel/src/main/java/com/example/samuraitravel/controller/AdminHouseController.java
@@ -1,7 +1,9 @@
 package com.example.samuraitravel.controller;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,17 +16,17 @@ import com.example.samuraitravel.repository.HouseRepository;
 @RequestMapping("/admin/houses")
 public class AdminHouseController {
 	private final HouseRepository houseRepository;
-	
+
 	public AdminHouseController(HouseRepository houseRepository) {
 		this.houseRepository = houseRepository;
 	}
-	
+
 	@GetMapping
-	public String index(Model model) {
-		List<House> houses = houseRepository.findAll();
-		
-		model.addAttribute("houses", houses);
-		
+	public String index(Model model, @PageableDefault(page = 0, size = 10, sort = "id", direction = Direction.ASC) Pageable pageable) {
+		Page<House> housePage = houseRepository.findAll(pageable);
+
+		model.addAttribute("housePage", housePage);
+
 		return "admin/houses/index";
 	}
 }
