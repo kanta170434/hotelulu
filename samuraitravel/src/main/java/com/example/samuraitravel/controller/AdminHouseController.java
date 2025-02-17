50905ee198fb2b40aca9b7f183d6fda758aff5d5
kantataguchi <kanta170434@gmail.com>
2025-02-17 10:12:23 +0900

    2月16日分コミット

--- samuraitravel/src/main/java/com/example/samuraitravel/controller/AdminHouseController.java
+++ samuraitravel/src/main/java/com/example/samuraitravel/controller/AdminHouseController.java
@@ -1,3 +1,5 @@
+//13-4から
+
 package com.example.samuraitravel.controller;
 
 import org.springframework.data.domain.Page;
@@ -6,22 +8,30 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.samuraitravel.entity.House;
 import com.example.samuraitravel.form.HouseRegisterForm;
 import com.example.samuraitravel.repository.HouseRepository;
+import com.example.samuraitravel.service.HouseService;
 
 @Controller
 @RequestMapping("/admin/houses")
 public class AdminHouseController {
 	private final HouseRepository houseRepository;
+	private final HouseService houseService;
 
-	public AdminHouseController(HouseRepository houseRepository) {
+	public AdminHouseController(HouseRepository houseRepository, HouseService houseService) {
 		this.houseRepository = houseRepository;
+		this.houseService = houseService;
 	}
 
 	@GetMapping
@@ -55,5 +65,18 @@ public class AdminHouseController {
 		model.addAttribute("houseRegisterForm", new HouseRegisterForm());
 
 		return "admin/houses/register";
+	}
+
+	@PostMapping("/create")
+	public String create(@ModelAttribute @Validated HouseRegisterForm houseRegisterForm, BindingResult bindingResult,
+			RedirectAttributes redirectAttributes) {
+		if (bindingResult.hasErrors()) {
+			return "admin/houses/register";
+		}
+
+		houseService.create(houseRegisterForm);
+		redirectAttributes.addFlashAttribute("successMessage", "民宿を登録しました。");
+
+		return "redirect:/admin/houses";
 	}
 }
